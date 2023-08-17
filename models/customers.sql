30190512a3a4b91ae770510ff26ecf6445d150fe
Pratik <pratik.mallya@gmail.com>
2023-08-17 09:26:23 -0700

    Update customers.sql

--- models/customers.sql
+++ models/customers.sql
@@ -49,8 +49,8 @@ final as (
 
     select
         customers.customer_id,
-        customers.first_name,
         customers.last_name,
+        customers.first_name,
         customer_orders.first_order,
         customer_orders.most_recent_order,
         customer_orders.number_of_orders,
