4ad2aa96b0ab7ccc5c5651a5f9bff11822d9f8b5
Pratik <pratik.mallya@gmail.com>
2025-05-09 11:03:14 -0500

    Update customers.sql

--- models/customers.sql
+++ models/customers.sql
@@ -27,7 +27,8 @@ customer_orders as (
 
         min(order_date) as first_order,
         max(order_date) as most_recent_order,
-        count(order_id) as number_of_orders
+        count(order_id) as number_of_orders,
+        count(order_id) as ci_number_of_orders
     from orders
 
     group by customer_id
